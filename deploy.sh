205922c9e5dd41d0abe19d21a720c29ee9fb5647
Dom Slee <domslee97@gmail.com>
2021-06-19 01:21:36 +1000

    idk

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+# TODO add assertions that the git status is empty
+# (otherwise lose work in progress)
 sed -i.bak "s/dist\/js\///" ".gitignore"
 git add dist
 git commit -m "add dist"
